7da02ef6f8c90dbba07386a2ff6936513da33de6
aughostinos <augustinos.nabil@gmail.com>
2023-11-28 15:22:39 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 99:
-        print(f"{i:02d},".format(), end=' ')
+        print("{i:02d},".format(), end=' ')
     else:
-        print(f"{i:02d}".format(), end=' ')
+        print("{i:02d}".format(), end=' ')
     i += 1
