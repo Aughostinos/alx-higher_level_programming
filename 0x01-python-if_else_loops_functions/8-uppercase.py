cd7ca706e77c6ea2273077493d147bb5c80fc178
aughostinos <augustinos.nabil@gmail.com>
2023-11-28 16:25:48 -0800

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_str = ""
+    for c in str:
+        if 'a' <= c <= 'z':
+            uppercase_str += chr(ord(c) - 32)
+        else:
+            uppercase_str += chr
+            
+    print(uppercase_str)
