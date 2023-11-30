963117f15880a88f6140b169251dfebe81f5f12a
aughostinos <augustinos.nabil@gmail.com>
2023-11-30 15:35:41 -0800

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import sys
-def print_argument(argv):
-    arg_number = len(argv)
+
+if __name__ == "__main__":
+    arg_number = len(sys.argv)
 
     if arg_number == 0:
         print("0 argument.")
     elif arg_number == 1:
-        print(f"1 argument:\n1: {argv[0]}".format())
+        print(f"1 argument:\n1: {sys.argv[1]}".format())
     else:
-        print(f"{arg_number} arguments:")
-        for i in argv:
-            print(f"{i}: {argv[1]}".format())
-
-    if __name__ == "__main__":
-        arguments = sys.argv[1:]
-        print_argument(arguments)
+        print(f"{arg_number} arguments:".format())
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}".format())
