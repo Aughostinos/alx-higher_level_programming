4f42117be44b5540dcc0f5ec58c864d15bfa9400
aughostinos <augustinos.nabil@gmail.com>
2023-12-02 09:38:23 -0800

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-        result = 0
-        for arg in sys.argv[1:]:
-            result += int(arg) 
-        print ("{}".format(result))
+    result = 0
+    for arg in sys.argv[1:]:
+        result += int(arg)
+        print("{}".format(result))
