c99935d89f3104b3b07ff13594cafcaed98c605b
aughostinos <augustinos.nabil@gmail.com>
2023-12-06 14:10:06 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,4 +5,4 @@ def search_replace(my_list, search, replace):
     for i in range(len(my_list)):
         if my_list[i] == search:
             my_list[i] = replace
-            return my_list
+    return my_list
