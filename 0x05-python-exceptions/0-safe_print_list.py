d3d3a3c694d1ea7d63a98213e7987aefe9a29635
aughostinos <augustinos.nabil@gmail.com>
2023-12-18 16:35:31 -0800

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+
+    for element in range(x):
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            print("index error")
+    return counter
