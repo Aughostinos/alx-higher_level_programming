540c574d83eab48c71dbd75ab4e99b6be0296f87
aughostinos <augustinos.nabil@gmail.com>
2023-12-18 17:34:44 -0800

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        return result
     except (ZeroDivisionError):
         print("Inside result: None")
         return None
     finally:
         print("Inside result: {}".format(result))
+        return result
