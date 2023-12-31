6e56af804d7b9bfc5580a1cab2abe129734d8435
aughostinos <augustinos.nabil@gmail.com>
2023-12-31 12:56:09 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a module that defines the square class
+"""
+
+
+class Square:
+
+    """
+    This is a class that defines a square with its size.
+
+    Attributes:
+    __size: size of the square.
+    """
+    def __int__(self, size):
+
+        """
+        Args:
+        size: private attrinute
+        """
+        self.__size = size
