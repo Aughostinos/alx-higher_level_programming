5ea15da7ed073b6861fbc7f7706f10e662869251
aughostinos <augustinos.nabil@gmail.com>
2024-01-08 18:56:13 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This is geometry module"""
+
+
+class BaseGeometry:
+    """
+    This is Geometry class
+    """
+
+    def area(self):
+        """
+        Public instance method
+        """
+        raise Exception("area() is not implemented")
