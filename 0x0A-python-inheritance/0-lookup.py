3d6e56d48b96de87594563e84db210f81ce0d13e
aughostinos <augustinos.nabil@gmail.com>
2024-01-08 18:00:57 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This is module  documentation"""
 
+
 def lookup(obj):
-    """ This  function returns the list of available 
+    """ This  function returns the list of available
     attributes and methods of an object"""
 
     return dir(obj)
