94aa7a9da1a1bb46aa710d89dcc0d0287fc5c316
aughostinos <augustinos.nabil@gmail.com>
2024-01-08 19:00:14 -0800

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""This is module  documentation"""
 
+
+class MyList(list):
+    """class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """
+        Public instance method
+        """
+
+        print(sorted(self))
