956bc66a65b536c045c6f9273ccca2c0f8e32d2c
aughostinos <augustinos.nabil@gmail.com>
2024-03-11 05:41:54 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let args = process.argv.length;
+
+if (args <= 2) {
+	console.log('No argument');
+} else if (args === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Aruments found');
+}
