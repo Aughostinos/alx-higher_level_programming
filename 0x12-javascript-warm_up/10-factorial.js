3b004da2956fab9fd774e4d7732e1f44e1c2e233
aughostinos <augustinos.nabil@gmail.com>
2024-03-11 20:11:37 -0700

    fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-function factorial(n) {
+function factorial (n) {
   if (isNaN(n) || n <= 0) { // Base case for NaN or non-positive numbers
     return 1;
   } else if (n === 1) { // Base case for factorial
@@ -13,4 +13,3 @@ function factorial(n) {
 const n = parseInt(process.argv[2]);
 
 console.log(factorial(n));
-
