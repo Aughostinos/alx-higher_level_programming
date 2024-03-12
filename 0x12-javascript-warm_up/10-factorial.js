a2e79cd9c08cf5d8cc00eed7485aded22896c627
aughostinos <augustinos.nabil@gmail.com>
2024-03-11 20:00:15 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function factorial(n) {
+  if (isNaN(n) || n <= 0) { // Base case for NaN or non-positive numbers
+    return 1;
+  } else if (n === 1) { // Base case for factorial
+    return 1;
+  } else {
+    return n * factorial(n - 1); // Recursive step
+  }
+}
+
+const n = parseInt(process.argv[2]);
+
+console.log(factorial(n));
+
