e91a030f766d602ed418099f23457a69143d7e0b
aughostinos <augustinos.nabil@gmail.com>
2024-03-11 19:53:02 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,10 +4,9 @@ const x = parseInt(process.argv[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
- let i = 0;
+  let i = 0;
   while (i < x) {
     console.log('C is fun');
     i++;
-
   }
 }
