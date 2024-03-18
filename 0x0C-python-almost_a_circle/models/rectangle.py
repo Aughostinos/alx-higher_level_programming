12b4fec8b3aee933ec1cb900c4bf070e6395d0f4
aughostinos <augustinos.nabil@gmail.com>
2024-03-18 02:31:57 -0700

    rect 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -48,6 +48,14 @@ class Rectangle(Base):
                 disp += '#'
             print(disp)
 
+    def __str__(self):
+        return ('[Rectangle] ({}) {}/{} - {}/{}'
+                .format(self.id,
+                        self.__x,
+                        self.__y,
+                        self.__width,
+                        self.__height))
+
     @property
     def width(self):
         return self.__width
