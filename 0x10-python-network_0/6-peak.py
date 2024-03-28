bb9e213b8da8fe7594e94fcb933e0de462cb43fb
aughostinos <augustinos.nabil@gmail.com>
2024-03-27 19:46:50 -0700

    fix

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""find a peak"""
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of integers"""
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    else:
+        return max(list_of_integers)
