a34d48b62ee34f651b27113b431adb15b4643e13
aughostinos <augustinos.nabil@gmail.com>
2024-03-28 19:54:39 -0700

    5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     url = sys.argv[1]
 
-    response = request.get(url)
-    x_request_id = response.headers.get('X-Request-Id')
+    response = requests.get(url)
+    x_req_id = response.headers.get('X-Request-Id')
 
     print(x_req_id)
