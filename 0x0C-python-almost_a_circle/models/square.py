94cb860d77e08ab795996858655fe09d106d9329
aughostinos <augustinos.nabil@gmail.com>
2024-04-03 06:25:03 -0700

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,8 +9,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
-
-    super().__init__(width=size, height=size, x=x, yxy, id=id)
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
 
     def __str__(self):
         """Return the string representation of the Square."""
