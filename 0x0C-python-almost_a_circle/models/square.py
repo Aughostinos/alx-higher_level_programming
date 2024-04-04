b9613da1c51ffdfdf933c94b68f0e904b0b91c84
aughostinos <augustinos.nabil@gmail.com>
2024-04-03 18:38:49 -0700

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,13 +11,30 @@ class Square(Rectangle):
         """Class constructor"""
         super().__init__(width=size, height=size, x=x, y=y, id=id)
 
-    def __str__(self):
-        """Return the string representation of the Square."""
-        return ('[Square] ({}) {}/{} - {}'
-                .format(self.id,
-                        self.x,
-                        self.y,
-                        self.width))
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute, in order."""
+        if len(args) > 0:
+            attr_order = ['id',
+                          'size',
+                          'x',
+                          'y']
+
+        for i, arg in enumerate(args):
+            if i < len(attr_order):
+                setattr(self, attr_order[i], arg)
+
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+        def __str__(self):
+            """Return the string representation of the Square."""
+            return ('[Square] ({}) {}/{} - {}'
+                    .format(self.id,
+                            self.x,
+                            self.y,
+                            self.width))
 
     @property
     def size(self):
