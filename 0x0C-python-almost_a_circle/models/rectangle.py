a42a3a0a7975c6543fb95294b2a7f7f5bffb3899
aughostinos <augustinos.nabil@gmail.com>
2024-04-03 19:39:42 -0700

    update Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,15 +73,15 @@ class Rectangle(Base):
                 if hasattr(self, key):
                     setattr(self, key, value)
 
-        def to_dictionary(self):
-            """public method that returns the dictionary representation of a Rectangle"""
-            return {
-                'id': self.id,
-                'width': self.width,
-                'height': self.height,
-                'x': self.x,
-                'y': self.y
-            }
+    def to_dictionary(self):
+        """public method that returns the dictionary representation of a Rectangle"""
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
 
     @property
     def width(self):
