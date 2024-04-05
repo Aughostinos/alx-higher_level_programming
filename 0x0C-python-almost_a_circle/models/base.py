1c5551d7f4fa33e2e98adf85c9f76c295bf1ce00
aughostinos <augustinos.nabil@gmail.com>
2024-04-05 05:24:17 -0700

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,6 +57,10 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
-        dummy_instance = cls(1)
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(2, 2)
+        elif cls.__name__ == "Square":
+            dumy_instance = cls(2)
+
         dummy_instance.update(**dictionary)
         return dummy_instance
