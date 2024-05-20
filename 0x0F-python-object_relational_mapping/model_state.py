a1263e419aee9870688a920d8c39f7b3c5718d93
Augustinos <augustinos.nabil@gmail.com>
2024-05-20 15:47:04 -0700

    fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,11 +6,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """ State class inherits from Base links to MySQL table states"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
 
 if __name__ == "__main__":
 
@@ -18,6 +20,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database_name = sys.argv[3]
 
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}', pool_pre_ping=True)
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}
+                           @localhost:3306/{database_name}',
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
