0c61938644d3ce0c565850af614d32cae8e55bf0
Augustinos Abusaif <augustinos.nabil@gmail.com>
2024-06-07 17:29:16 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,10 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
-const stringToWrite = process.argv[3]
+const stringToWrite = process.argv[3];
 
 fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
   if (err) {
     console.log(err);
-    return;
   }
 });
