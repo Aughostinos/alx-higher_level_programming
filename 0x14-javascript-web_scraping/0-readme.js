ae241abc69e9ca5c9afa51ca1cc968c691e7c896
Augustinos Abusaif <augustinos.nabil@gmail.com>
2024-06-07 17:12:57 -0700

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,10 +6,10 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8',  (err, date) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(date);
-    }
+fs.readFile(filePath, 'utf8', (err, date) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(date);
+  }
 });
