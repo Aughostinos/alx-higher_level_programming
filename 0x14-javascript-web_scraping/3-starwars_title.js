8546a6a0e198b657a221d0a6121b553a2a51b8d6
Aughostinos <augustinos.nabil@gmail.com>
2024-06-08 05:24:28 +0400

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+/**
+ * This script prints the title of a Star Wars movie where the episode number matches a given integer.
+ */
+
+const request = require('request');
+const movie_id = process.argv[2];
+const api_url = 'https://swapi-api.alx-tools.com/api/films/${movie_id}';
+
+request.get(api_url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    console.log(movie.title)
+  }
+});
