17b7b523a64553bc8868d2e9a4b95b0fe5820122
Aughostinos <augustinos.nabil@gmail.com>
2024-06-10 04:37:28 +0400

    5-request_store.js.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/**
+ * this script writes a string to a file.
+ */
+
+const fs = require('fs');
+const url = process.argv[2]
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if(error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+  });
+}
+});
